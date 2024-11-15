10efb833de93e030ecc773a3e8a160ca4d9a192d
Kenn-Michael Sanga <kennsanga@gmail.com>
2024-11-15 09:50:10 +0100

    draw.io uml class diagram

--- MTCG_Project/MTCG_Project/obj/Debug/net8.0/MTCG_Project.AssemblyInfo.cs
+++ MTCG_Project/MTCG_Project/obj/Debug/net8.0/MTCG_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTCG_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03825c3aa76a505a4c34f81ddbd6f128a868c1c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6672d61ae8cb2d3a758851c5b5783146fb719ccc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTCG_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTCG_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
