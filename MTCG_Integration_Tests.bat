e6a9cd8ec477ac9267eefc7059a5e753533c9831
Kenn-Michael Sanga <kennsanga@gmail.com>
2024-11-25 19:09:39 +0100

    updated integration tests

--- MTCG_Integration_Tests.bat
+++ MTCG_Integration_Tests.bat
@@ -1,12 +1,40 @@
-# create/register users
-curl -i -X POST http://localhost:12000/users --header "Content-Type: application/json" -d "{\"username\":\"msmith\", \"password\":\"sample2\", \"name\":\"Mary Smith\", \"email\":\"mary.smith@example.com\"}"
+echo ========================================
+echo create/register users
+curl -i -X POST http://localhost:12000/users --header "Content-Type: application/json" -d "{\"username\":\"ksanga\", \"password\":\"sample2\", \"name\":\"Mary Smith\", \"email\":\"mary.smith@example.com\"}"
 curl -i -X POST http://localhost:12000/users --header "Content-Type: application/json" -d "{\"username\":\"jdoe\", \"password\":\"example1\", \"name\":\"John Doe\"}"
 
-# register fail
-curl -i -X POST http://localhost:12000/users --header "Content-Type: application/json" -d "{\"username\":\"jdoe\", \"password\":\"none\", \"name\":\"Oswin Oswald\"}"
+echo ========================================
+echo register fail
+curl -i -X POST http://localhost:12000/users --header "Content-Type: application/json" -d "{\"username\":\"ksanga\", \"password\":\"none\", \"name\":\"Kenn Sanga\"}"
 
-# login
-curl -i -X POST http://localhost:12000/sessions --header "Content-Type: application/json" -d "{\"username\":\"jdoe\", \"password\":\"example1\"}"
+echo ========================================
+echo login and get token
+curl -i -X POST http://localhost:12000/sessions --header "Content-Type: application/json" -d "{\"username\":\"ksanga\", \"password\":\"sample2\"}"
 
-# get all users
-curl -i -X GET http://localhost:12000/users --header "Content-Type: application/json"
+echo ========================================
+echo login fail
+curl -i -X POST http://localhost:12000/sessions --header "Content-Type: application/json" -d "{\"username\":\"jdoe\", \"password\":\"fail\"}"
+
+echo ========================================
+echo get user ksanga
+curl -i -X GET http://localhost:12000/users/ksanga --header "Content-Type: application/json" --header "Authorization: Bearer ksanga-debug"
+
+echo ========================================
+echo update user ksanga
+curl -i -X PUT http://localhost:12000/users/ksanga --header "Content-Type: application/json" --header "Authorization: Bearer ksanga-debug" -d "{\"password\":\"example1\", \"name\":\"Kenn Sanga\"}"
+
+echo ========================================
+echo get user ksanga
+curl -i -X GET http://localhost:12000/users/ksanga --header "Content-Type: application/json" --header "Authorization: Bearer ksanga-debug"
+
+echo ========================================
+echo update user ksanga username
+curl -i -X PUT http://localhost:12000/users/ksanga --header "Content-Type: application/json" --header "Authorization: Bearer ksanga-debug" -d "{\"username\":\"fredz\", \"name\":\"Fred Zinnemann\"}"
+
+echo ========================================
+echo get user ksanga fail
+curl -i -X GET http://localhost:12000/users/ksanga --header "Content-Type: application/json" --header "Authorization: Bearer ksanga-debug"
+
+echo ========================================
+echo get user fredz
+curl -i -X GET http://localhost:12000/users/fredz --header "Content-Type: application/json" --header "Authorization: Bearer fredz-debug"
